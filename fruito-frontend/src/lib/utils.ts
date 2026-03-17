bfe7bd4a5d939901b31d68d709a7ff6295a765f8
piyushpatelPP <piyushpatel52319@gmail.com>
2026-03-17 22:30:44 +0530

    removed frontend folder from backend repo

--- fruito-frontend/src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-export const formatCurrency = (value: number): string => {
-  return new Intl.NumberFormat('en-IN', {
-    style: 'currency',
-    currency: 'INR',
-    minimumFractionDigits: 2,
-  }).format(value);
-};
